c7295a9e0a7aae6add74de38fd4920a762d5b35e
thejakgithub <jak171646167@gmail.com>
2021-11-13 20:23:15 +0700

    update component list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -6,8 +6,8 @@ export const List: FC<SeriesProps> = ({ seriesList }) => {
     <div className="series-list">
       {seriesList.map((series, idx) => (
         <div key={idx} className="series-item">
-            <div className="series-cover">
-          <img src={series.cover} alt="series-cover" />
+          <div className="series-cover">
+            <img src={series.cover} alt="series-cover" />
           </div>
           <p>
             <b> Name : {series.name}</b>
@@ -19,7 +19,7 @@ export const List: FC<SeriesProps> = ({ seriesList }) => {
           {series.imdb === "2" && <p>⭐⭐</p>}
           {series.imdb === "3" && <p>⭐⭐⭐</p>}
           {series.imdb === "4" && <p>⭐⭐⭐⭐</p>}
-          {series.imdb === "5" && <p>⭐⭐⭐⭐⭐</p>}
+          {series.imdb >= "5" && <p>⭐⭐⭐⭐⭐</p>}
         </div>
       ))}
     </div>
